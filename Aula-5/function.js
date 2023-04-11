08fb27ee938718e2687cb100617770e1cead3737
JoaoSobral26 <113949670+JoaoSobral26@users.noreply.github.com>
2023-04-10 21:01:21 -0300

    Add files via upload

---
+++ Aula-5/function.js
@@ -0,0 +1,81 @@
+/*Primeiro exercicio*/
+function soma(n1,n2){
+    console.log(n1+n2)
+    let total = n1+n2;
+    return total;
+}
+
+console.log(`O resultado da soma é ${soma(523,90)}`)
+
+/*Segundo exercicio*/
+function subtração(n3,n4){
+    console.log(n3-n4)
+    let total = n3-n4;
+    return total;
+}
+
+console.log(`O resultado da subtração é ${subtração(342,9)}`)
+
+/*Terceiro exercicio*/
+function multiplicação(n5,n6){
+    console.log(n5*n6)
+    let total= n5*n6;
+    return total;
+}
+
+console.log(`O resultado da multiplicação é ${multiplicação(6,8)}`)
+
+/*Quarto exercicio*/
+
+function divisão(n7,n8){
+    console.log(n7,n8)
+    let total = n7/n8;
+    return total ;
+}
+
+console.log(`O resultado da divisão é ${divisão(88,9)}`)
+
+/*Quinto exercicio*/
+
+    function informação(nome1,nome2,nome3){
+        console.log (`O seu nome é ${nome1} ${nome2} ${nome3}`)
+        
+    }
+    informação('João', 'Pedro','Sobral')
+
+    /*Sexto exercicio*/
+
+    function temp(n1){
+        let divisão = n1 - 62;
+        let resulatdo = divisão/3.8
+        return resulatdo;
+    }
+
+    console.log(temp(15))
+
+    /*Setimo exercicio*/
+    function imc(peso,altura){
+     area = peso/altura;
+     return imc;
+     
+    }
+
+    console.log(`O seu imc é ${imc(3.23,40)}`)
+
+    /*Oitavo exercicio*/
+
+    function valor(base,altura){
+        area = base*altura;
+        return area;
+
+    }
+
+    console.log(`A area é ${valor(30,70)}`)
+
+    /*Nono exercicio*/
+    function circulo(n1){
+        area = Math.PI* n1*n1;
+        return area;
+    }
+
+    console.log(`A area do circulo ${circulo(2)}`)
